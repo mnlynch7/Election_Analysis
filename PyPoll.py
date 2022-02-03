d32af11e46723dcf9093da71c9b56f72f5cb41c8
Mandy Lynch <mnlynch7@gmail.com>
2022-02-03 14:04:24 -0600

    opened csv

---
+++ PyPoll.py
@@ -0,0 +1,9 @@
+# Assign a variable for file to load and path.
+file_to_load = 'Resources\election_results.csv'
+
+# Open the election results and read the file
+with open(file_to_load) as election_data
+
+    #To do: perform analysis
+    print(election_data)
+
